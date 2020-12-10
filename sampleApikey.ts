b80fd4bef4ea0f0771a9f27306d13e574ec9ad17
pavaimalar <75773488+pavaimalar@users.noreply.github.com>
2020-12-10 19:22:18 +0530

    Add files via upload

---
+++ sampleApikey.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyCMwZtcOYF010CKPuvBkkZZme8jTM81LvE",
+      authDomain: "example1-67bf8.firebaseapp.com",
+      databaseURL: "https://example1-67bf8.firebaseio.com",
+      projectId: "example1-67bf8",
+      storageBucket: "example1-67bf8.appspot.com",
+      messagingSenderId: "628740113340",
+      appId: "1:628740113340:web:7c07eac6716fccccd3a1b9",
+      measurementId: "G-16RS2TFF5D"
+    }
+  };
+  
